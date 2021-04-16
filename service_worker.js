80d492de0394b44d5984d9f742633bd22ca436c3
Mawsn <54127173+Mawsn@users.noreply.github.com>
2021-04-16 10:42:38 +1000

    .

--- service_worker.js
+++ service_worker.js
@@ -3,10 +3,3 @@ workbox.routing.registerRoute(
 	({request}) => request.destination == 'image',
 	new workbox.strategies.CacheFirst()
 );
-self.addListener("fetch",e =>{
-	e.respondWith(
-		fetch(e.request).catch(() =>{
-			return new Response("Hello")
-		})
-	);
-});
