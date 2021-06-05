e92e2d739eeb61ffced6a2370506e29a70676ebf
elisha.mawson14 <54127173+Mawsn@users.noreply.github.com>
2021-06-05 16:25:30 +1000

    Google Sign In Experimentation

--- assets/js/onboarding.js
+++ assets/js/onboarding.js
@@ -99,3 +99,19 @@ function createUser() {
 
 	});
 }
+function signinWithGoogle(){
+	console.log("Signin with Google");
+	var provider = new firebase.auth.GoogleAuthProvider();
+	firebase.auth().signInWithPopup(provider)
+  	.then((result) => {
+	    var credential = result.credential;
+	    var token = credential.accessToken;
+	    var user = result.user;
+
+  	}).catch((error) => {
+	    var errorCode = error.code;
+	    var errorMessage = error.message;
+	    var email = error.email;
+	    var credential = error.credential;
+  	});
+}
