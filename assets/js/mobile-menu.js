71d79dacad08d7a1f29efcd5545467c502885019
Mawsn <54127173+Mawsn@users.noreply.github.com>
2021-07-10 20:07:26 +1000

    Mobile menu completed, profile icon added

--- assets/js/mobile-menu.js
+++ assets/js/mobile-menu.js
@@ -1,14 +1,12 @@
-function expandMenu(){
-	var x = document.getElementById("menu-fragment");
-	if (x.style.display === "block") {
-		x.style.display = "none";
-	} 
-	else {
-	    x.style.display = "block";
-	}
-}
+//Window resize listener is used to automatically close the mobile menu when the window becomes desktop sized 
 window.addEventListener('resize', function(event) {
     if(window.innerWidth >= 767){
         document.getElementById("menu-fragment").style.display = "none";
     }
 }, true);
+//JQuery, used to handle the opening and closing of the mobile menu 
+$(document).ready(function(){
+  	$("#mobile-menu-icon").click(function(){
+    	$("#menu-fragment").slideToggle("slow");
+  	});
+});
