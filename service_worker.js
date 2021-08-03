a74221f981022f7ddbe290bba07819cede8d0aaf
Mawsn <54127173+Mawsn@users.noreply.github.com>
2021-08-03 18:32:35 +1000

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -5,6 +5,23 @@ workbox.routing.registerRoute(
 	new workbox.strategies.CacheFirst()
 );
 
-window.addEventListener('appinstalled', function() {
-  console.log('Thank you for installing our app!');
-});
+let isInstalled = localStorage.getItem('pwaInstalled') === '1' || false;
+
+if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
+    // User is currently navigating on the PWA so yes it's installed
+    localStorage.setItem('pwaInstalled', '1');
+    isInstalled = true;
+} else {
+    //User is navigating in browser
+    window.addEventListener('beforeinstallprompt', () => {
+        localStorage.setItem('pwaInstalled', '0');
+        isInstalled = false;
+        //User can get an installation prompt meaning the app is not installed
+    });
+    window.addEventListener('onappinstalled', () => {
+        localStorage.setItem('pwaInstalled', '1');
+        isInstalled = true;
+    });
+}
+
+window.alert(isInstalled);
