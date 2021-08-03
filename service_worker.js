7bf5759460b5b05467cac1cf70ac9da6ae2d1542
Mawsn <54127173+Mawsn@users.noreply.github.com>
2021-08-03 18:41:31 +1000

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -1,27 +1,10 @@
-
+/*
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js');
 workbox.routing.registerRoute(
 	({request}) => request.destination == 'image',
 	new workbox.strategies.CacheFirst()
-);
+);*/
 
-let isInstalled = localStorage.getItem('pwaInstalled') === '1' || false;
-
-if (window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true) {
-    // User is currently navigating on the PWA so yes it's installed
-    localStorage.setItem('pwaInstalled', '1');
-    isInstalled = true;
-} else {
-    //User is navigating in browser
-    window.addEventListener('beforeinstallprompt', () => {
-        localStorage.setItem('pwaInstalled', '0');
-        isInstalled = false;
-        //User can get an installation prompt meaning the app is not installed
-    });
-    window.addEventListener('onappinstalled', () => {
-        localStorage.setItem('pwaInstalled', '1');
-        isInstalled = true;
-    });
-}
-
-console.log(isInstalled);
+self.addEventListener("fetch", event => {
+    console.log("You fetched " + event.url);
+});
